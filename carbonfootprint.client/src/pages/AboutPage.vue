812f43cb9e12083bc089132fe2deb62825682523
alec victory <alecvictory93@gmail.com>
2021-05-10 09:39:46 -0600

    puuuuuuuuuuuuuush

--- carbonfootprint.client/src/pages/AboutPage.vue
+++ carbonfootprint.client/src/pages/AboutPage.vue
@@ -1,6 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="about container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h1>About *insert company name here*</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h2>
+          Vision Statement
+        </h2>
+      </div>
+      <div class="col-12">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi neque placeat voluptatem dolorem officiis doloremque ea voluptatum. Iste voluptas cum, beatae illum consectetur tenetur laboriosam expedita id a voluptates modi!
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h2>
+          How to use
+        </h2>
+      </div>
+      <div class="col-12">
+        info 1
+      </div>
+      <div class="col-12">
+        info 2
+      </div>
+      <div class="col-12">
+        info 3
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +41,8 @@ export default {
   name: 'AboutPage'
 }
 </script>
+<style lang="scss" scoped>
+  *{
+    border: solid 1px red;
+  }
+</style>
--- carbonfootprint.client/src/pages/ProfilePage.vue
+++ carbonfootprint.client/src/pages/ProfilePage.vue
@@ -1,6 +1,35 @@
 <template>
-  <div class="profile-page">
-    this is the profile page
+  <div class="profile-page container-fluid">
+    <div class="row">
+      <div class="col-12">
+        This Week's Score
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <img src="http://placehold.it/200x200" alt="">
+      </div>
+      <div class="col-12">
+        Your Scores for the Week
+        <button>Today's Emissions data</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        User's Profile
+      </div>
+      <div class="col-12">
+        Location:
+      </div>
+      <div class="col-12">
+        Info
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <button>edit profile</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +44,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  *{
+    border: solid 1px red;
+  }
 </style>
