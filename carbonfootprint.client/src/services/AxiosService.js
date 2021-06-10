d36b2bc1b30a503f1cfed49ad42e067de1840ced
austinjdowney <austinjdowney@gmail.com>
2021-06-10 10:22:26 -0600

    increase timeout

--- carbonfootprint.client/src/services/AxiosService.js
+++ carbonfootprint.client/src/services/AxiosService.js
@@ -6,11 +6,11 @@ export const api = Axios.create({
 })
 export const weatherApi = Axios.create({
   baseURL: 'https://api.airvisual.com/v2/city?city=Boise&state=Idaho&country=USA&key=040cdc41-fc2a-44dd-a696-5d6245acce0e',
-  timeout: 8000
+  timeout: 15000
 })
 export const newsApi = Axios.create({
   baseURL: 'https://custom-search.p.rapidapi.com/api/search/CustomNewsSearchAPIV2?q=Carbon Emissions&searchEngineId=687417457347224395&pageNumber=1',
-  timeout: 8000,
+  timeout: 15000,
   headers: {
     'x-rapidapi-key': '8bcd6e56a5mshdbc3fe3aad9cc9dp15aa46jsn21976a70e5e7',
     'x-rapidapi-host': 'custom-search.p.rapidapi.com',
