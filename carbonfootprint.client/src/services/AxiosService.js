71aa272792ad526386e2b5e26ece2d42477483db
Austin Downey <77709847+austinjdowney@users.noreply.github.com>
2025-08-28 20:22:37 -0700

    Update AxiosService.js

--- carbonfootprint.client/src/services/AxiosService.js
+++ carbonfootprint.client/src/services/AxiosService.js
@@ -12,7 +12,7 @@ export const newsApi = Axios.create({
   baseURL: 'https://custom-search.p.rapidapi.com/api/search/CustomNewsSearchAPIV2?q=Carbon Emissions&searchEngineId=687417457347224395&pageNumber=1',
   timeout: 15000,
   headers: {
-    'x-rapidapi-key': '8bcd6e56a5mshdbc3fe3aad9cc9dp15aa46jsn21976a70e5e7',
+    'x-rapidapi-key': 'getNewApiKey',
     'x-rapidapi-host': 'custom-search.p.rapidapi.com',
     useQueryString: true
   }
